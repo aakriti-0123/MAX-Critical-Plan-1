f1b0827dcfacfffd9d4baf6daa9ec1f5a6b61a4a
aakriti-0123 <aakrit94@gmail.com>
2025-07-05 19:31:19 +0400

    Upload_Calendar.py

--- pages/1_Upload_Calendar.py
+++ pages/1_Upload_Calendar.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
 password = st.text_input("Enter Admin Password", type="password")
+admin_pass = st.secrets.get("ADMIN_PASS")
 
-if password == st.secrets.get("ADMIN_PASS"):
+# Debug lines (remove after testing)
+st.write(f"Typed password: `{password}`")
+st.write(f"Secret password: `{admin_pass}`")
+
+if admin_pass is None:
+    st.error("Secret ADMIN_PASS not set!")
+elif password == admin_pass:
     uploaded_files = st.file_uploader("Upload up to 3 Excel calendar files", type="xlsx", accept_multiple_files=True)
     if uploaded_files:
         if len(uploaded_files) > 3:
